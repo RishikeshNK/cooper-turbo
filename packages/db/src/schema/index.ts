1d2c44022f10001386610f54659838b7796497e7
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-07-22 19:27:38 -0400

    feat(db): add migrations for nextauth tables

---
+++ packages/db/src/schema/index.ts
@@ -0,0 +1,5 @@
+import * as account from "./account";
+import * as session from "./session";
+import * as user from "./user";
+
+export { account, session, user };
---
+++ packages/db/src/schema/user.ts
@@ -0,0 +1,19 @@
+import { relations } from "drizzle-orm";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+import { Account } from "./account";
+
+export const User = pgTable("user", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }),
+  email: varchar("email", { length: 255 }).notNull(),
+  emailVerified: timestamp("emailVerified", {
+    mode: "date",
+    withTimezone: true,
+  }),
+  image: varchar("image", { length: 255 }),
+});
+
+export const UserRelations = relations(User, ({ many }) => ({
+  accounts: many(Account),
+}));
