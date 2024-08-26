ea3b455ccd84c5f4aaefd08e3961c5f98fc59bd1
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-08-26 15:24:19 -0400

    chore: add some form-related ui components

---
+++ apps/web/src/components/form/review-form.tsx
@@ -0,0 +1,7 @@
+export const benefits = [
+  { field: "pto", label: "PTO" },
+  { field: "federalHolidays", label: "Federal holidays off" },
+  { field: "freeLunch", label: "Free lunch" },
+  { field: "freeTransport", label: "Free transportation" },
+  { field: "freeMerch", label: "Free merchandise" },
+];
