ba3852ed54110b8dc20537a70c8c9eca456f5161
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-08-26 16:09:40 -0400

    feat(app): migrate review form completely

---
+++ apps/web/src/components/form/sections/index.ts
@@ -0,0 +1,11 @@
+import { CoopCycleSection } from "~/components/form/sections//coop-cycle-section";
+import { RatingsSection } from "~/components/form/sections//ratings-section";
+import { ReviewSection } from "~/components/form/sections//review-section";
+import { CompanyDetailsSection } from "~/components/form/sections/company-details-section";
+
+export {
+  CoopCycleSection,
+  RatingsSection,
+  ReviewSection,
+  CompanyDetailsSection,
+};
