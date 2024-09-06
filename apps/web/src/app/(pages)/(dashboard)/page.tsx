e01431ab01efa16a3202455cf14e43c491935ff8
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-09-06 15:55:47 -0400

    chore: remove html tag

--- apps/web/src/app/(pages)/(dashboard)/page.tsx
+++ apps/web/src/app/(pages)/(dashboard)/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="flex h-[85vh] flex-col">
       <div className="flex h-full flex-col items-center justify-center">
-        <h1>TEST</h1>
         <SearchFilter />
       </div>
     </div>
